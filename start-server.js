fb9b99105cf1880d513976b9bb5b1bfea1e3477f
Tiago Gaze <tiago@gaze.dev>
2026-08-26 11:23:10 -0300

    atualiza geral

--- start-server.js
+++
@@ -1,6 +0,0 @@
-const app = require('./index');
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log('Servidor rodando em http://localhost:' + PORT);
-});
--- test-server.js
+++
@@ -1,2 +0,0 @@
-console.log('Test file:', require.main.filename);
-console.log('Index file:', require('./index.js') ? 'loaded' : 'not loaded');
